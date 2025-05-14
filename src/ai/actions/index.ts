56b3961c08859d85ffc2b40ca73b5bb1e61bd178
rickyli888 <rickyli860@gmail.com>
2025-05-13 20:50:46 -0400

    remove default actions

--- src/ai/actions/index.ts
+++ src/ai/actions/index.ts
@@ -1,13 +1,2 @@
 import type { AntispaceMetadata } from "@antispace/sdk";
 export * from "./spotify";
-
-export const setNickname = async (
-	nickname: string,
-	anti: AntispaceMetadata,
-) => {
-	// placeholder
-};
-
-export const sayNickname = async (anti: AntispaceMetadata) => {
-	// placeholder
-};
--- src/manifest/functions.ts
+++ src/manifest/functions.ts
@@ -1,49 +1,10 @@
 import type { AntispaceAppFunction } from "@antispace/sdk";
 
-/**
- * Function to set the user's nickname
- */
-export const set_nickname: AntispaceAppFunction<
-	"set_nickname",
-	{
-		nickname: string;
-	}
-> = {
-	type: "function",
-	function: {
-		name: "set_nickname",
-		description: "Set user's nickname",
-		parameters: {
-			type: "object",
-			properties: {
-				nickname: {
-					type: "string",
-					description: "Nickname string",
-				},
-			},
-			required: ["nickname"],
-		},
-	},
-};
-
 export const connect_spotify: AntispaceAppFunction<"connect_spotify"> = {
 	type: "function",
 	function: {
 		name: "connect_spotify",
 		description: "Initiates the process to connect the user's Spotify account.",
-		parameters: {
-			type: "object",
-			properties: {},
-			required: [],
-		},
-	},
-};
-
-export const say_nickname: AntispaceAppFunction<"say_nickname"> = {
-	type: "function",
-	function: {
-		name: "say_nickname",
-		description: "Echo back the user's nickname",
 		parameters: {
 			type: "object",
 			properties: {},
